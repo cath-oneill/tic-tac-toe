9c870261adbfb4e7bdea3717cf6fd61d000bf2e7
Catherine O'Neill <cathzozo@gmail.com>
2014-07-27 21:00:39 -0500

    added wins for diagonal and vertical

--- main.js
+++ main.js
@@ -28,10 +28,16 @@ var checkForWinner = function () {
   if ( spaces[0] === spaces[1] && spaces[1] === spaces[2]
     || spaces[3] === spaces[4] && spaces[4] === spaces[5]
     || spaces[6] === spaces[7] && spaces[7] === spaces[8]
-    // TODO: Check for rest of game winning cases
+    || spaces[0] === spaces[4] && spaces[4] === spaces[8]
+    || spaces[2] === spaces[4] && spaces[4] === spaces[6]
+    || spaces[0] === spaces[3] && spaces[3] === spaces[6]
+    || spaces[1] === spaces[4] && spaces[4] === spaces[7]
+    || spaces[2] === spaces[5] && spaces[5] === spaces[8]
   )
   {
     console.log('somebody won');
+    
+    .trigger('game-win', (e, winner));
     // TODO: Trigger 'game-win' event with the winning player as the event data
   }
 };
@@ -51,7 +57,7 @@ $(document).on('click', '#board .space', function (e) {
 });
 
 $(document).on('game-win', function (e, winner) {
-  // TODO: Alert who won the game
+  alert(winner);
 });
 
 // Start the game
